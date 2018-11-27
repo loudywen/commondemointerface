2f4b990fac7fa9c6f5af4f2b644f8cb9d639ceef
loudywen <loudywen@gmail.com>
2018-11-27 15:53:30 -0600

    add missing file

---
+++ src/main/java/com/devon/demo/common/Shape.java
@@ -0,0 +1,20 @@
+package com.devon.demo.common;
+
+/**
+ * The interface Shape.
+ */
+public interface Shape {
+    /**
+     * Transform shape.
+     *
+     * @return the shape
+     */
+    Shape transform();
+
+    /**
+     * Name string.
+     *
+     * @return the string
+     */
+    String name();
+}
